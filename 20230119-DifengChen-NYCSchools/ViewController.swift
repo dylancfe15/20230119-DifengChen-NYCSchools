1b3f849dcb183567f06f4131d511b544b71a5a39
Dylan Chen <dylancfe15@gmail.com>
2023-01-19 13:59:22 -0500

    Initial Commit

---
+++ 20230119-DifengChen-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20230119-DifengChen-NYCSchools
+//
+//  Created by Difeng Chen on 1/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 20230119-DifengChen-NYCSchoolsUITests/_0230119_DifengChen_NYCSchoolsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _0230119_DifengChen_NYCSchoolsUITestsLaunchTests.swift
+//  20230119-DifengChen-NYCSchoolsUITests
+//
+//  Created by Difeng Chen on 1/19/23.
+//
+
+import XCTest
+
+final class _0230119_DifengChen_NYCSchoolsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
